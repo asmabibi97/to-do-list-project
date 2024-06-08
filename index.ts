fe14910ca31e99cc08944c295195a92d59768f65
asmabibi97 <165086445+asmabibi97@users.noreply.github.com>
2024-06-08 01:46:00 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+let todos=[]
+let condition=true;
+while(condition)
+    {
+let addtask=await inquirer.prompt([
+    {
+        name:"todo",
+        type:"input",
+        message:"what you want to add in your list?",
+
+    },
+    {
+        name:"addmore",
+        type:"confirm",
+        message:"do you want to add more items in list?",
+    }
+]);
+
+todos.push(addtask.todo)
+condition=addtask.addmore;
+console.log(todos);
+    }
